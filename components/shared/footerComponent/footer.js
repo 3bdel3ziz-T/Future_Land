1279fd188e44ad4121cd7c1864374ad73f0ca635
3bdel3zizT <zizotamercomz@gmail.com>
2024-12-11 09:42:04 +0200

    '1- add the company name to the footer and make it dynamic to update the current year, 2- add the header links with slot to allow the navigation works with shadow dom'

--- components/shared/footerComponent/footer.js
+++ components/shared/footerComponent/footer.js
@@ -1,18 +1,24 @@
 export default class FooterComponent extends HTMLElement {
+	styleSheetPath = "./app/styles/output.css";
+	templatePath = "./components/shared/footerComponent/footer.html";
+	currentYear = new Date().getFullYear();
 	constructor() {
 		super();
+		this.attachShadow({ mode: "open" });
 	}
 
 	async connectedCallback() {
-		await this.renderView();
+		this.shadowRoot.innerHTML = `
+				<link rel="stylesheet" href="${this.styleSheetPath}">
+				${await this.renderView()}`;
+		const year = this.shadowRoot.querySelector("#currentYear");
+		year.textContent = this.currentYear;
 	}
 	async renderView() {
 		try {
-			const response = await fetch(
-				"./components/shared/footerComponent/footer.html"
-			);
+			const response = await fetch(`${this.templatePath}`);
 			const template = await response.text();
-			this.innerHTML = template;
+			return template;
 		} catch (error) {
 			console.error("Error loading footer template:", error);
 		}
--- components/shared/headerComponent/header.js
+++ components/shared/headerComponent/header.js
@@ -1,25 +1,26 @@
 export default class Header extends HTMLElement {
-    template;
-    constructor() {
-        super();
-        // this.attachShadow({ mode: 'open' });
-        this.innerHTML = this.template
-    }
-    
-    async connectedCallback() {
-        await this.renderView();
-    }
-    async renderView() {
-        try {
-            const response = await fetch('./components/shared/headerComponent/header.html');
-            const template = await response.text();
-            this.innerHTML = template;
-        } catch (error) {
-            console.error('Error loading header template:', error);
-        }
-    }}
+	styleSheetPath = "./app/styles/output.css";
+	templatePath = "./components/shared/headerComponent/header.html";
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+	}
+
+	async connectedCallback() {
+		this.shadowRoot.innerHTML = `
+				<link rel="stylesheet" href="${this.styleSheetPath}">
+				${await this.renderView()}`;
+	}
+	async renderView() {
+		try {
+			const response = await fetch(`${this.templatePath}`);
+			const template = await response.text();
+			return template;
+		} catch (error) {
+			console.error("Error loading footer template:", error);
+		}
+	}
+}
 
 // Register the custom element
-window.customElements.define('header-component', Header);
-
-// new Header();
+window.customElements.define("header-component", Header);
