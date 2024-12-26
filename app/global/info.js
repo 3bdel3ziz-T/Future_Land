ecd3cae1237eb487261136cf470758ed235ed280
3bdel3zizT <zizotamercomz@gmail.com>
2024-12-26 15:29:01 +0200

    add whatsapp link and make the links comes from info obj

--- app/global/info.js
+++ app/global/info.js
@@ -9,9 +9,14 @@ const companyInfo = {
 		zip: "10001",
 	},
 	contacts: {
-		countryCode: "+00 971",
-		phone: "50 962 2996",
+		countryCode: 971,
+		phone: 509622996,
 		email: "Info@futureslandfood.com",
+	},
+	socials: {
+		facebook: "https://www.facebook.com/futurelanddubai",
+		instagram: "https://www.instagram.com/futureslanddubai/",
+		whatsapp: "https://wa.me/971509622996",
 	},
 	working: {
 		timeFrom: "9am",
